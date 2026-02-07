08222f3c5e670d03f9b9bd0b1faf6d0dd45f83a6
samuel daniels <sd24434@essex.ac.uk>
2026-02-07 02:44:04 +0000

    added dashboard overview

--- app/extensions/queue.py
+++ app/extensions/queue.py
@@ -159,23 +159,3 @@ def get_rq_defaults() -> Dict[str, Any]:
     
     
     
-# # app/extensions/queue.py
-
-# import os
-# from rq import Queue
-# from rq_scheduler import Scheduler
-
-# from .redis_conn import redis_client
-
-# PUBLISH_QUEUE_NAME = os.getenv("RQ_PUBLISH_QUEUE", "publish")
-
-# publish_queue = Queue(PUBLISH_QUEUE_NAME, connection=redis_client)
-
-# # This scheduler stores scheduled jobs in Redis and later moves them into `publish_queue`
-# scheduler = Scheduler(queue=publish_queue, connection=redis_client)
-
-# def get_queue(name: str = None) -> Queue:
-#     if not name or name == PUBLISH_QUEUE_NAME:
-#         return publish_queue
-
-#     return Queue(name, connection=redis_client)
